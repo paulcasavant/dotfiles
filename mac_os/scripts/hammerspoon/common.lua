e695f054cf54a678ed33dbbe8b7cb0a42fba4f07
Paul Casavant <thepaulcasavant@gmail.com>
2024-10-22 15:01:41 -0700

    Add window close and focus

--- mac_os/scripts/hammerspoon/common.lua
+++ mac_os/scripts/hammerspoon/common.lua
@@ -1,5 +1,14 @@
 local common = {}
 
+function CloseCurrentWindow()
+    local win = hs.window.focusedWindow()
+    if win then
+        win:close()
+    else
+        hs.alert.show("No focused window!")
+    end
+end
+
 function common.Sleep(n)
   os.execute("sleep " .. tonumber(n))
 end
